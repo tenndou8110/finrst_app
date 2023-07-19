298ce0cbde67d7dbfbd3818239a9092e291d6c2e
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-07-19 18:30:46 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-  has_many :tweets
-  has_many :comments  # commentsテーブルとのアソシエーション
-
-  validates :nickname, presence: true
+  
+        validates :name, presence: true
 end
